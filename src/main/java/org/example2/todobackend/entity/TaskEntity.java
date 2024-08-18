0ef6bccf365b66f1a1e25ae6080cf9c9c0c6b896
Parami <paramiashinsana@gmail.com>
2024-08-19 01:48:48 +0530

    update TaskEntity

--- src/main/java/org/example2/todobackend/entity/TaskEntity.java
+++ src/main/java/org/example2/todobackend/entity/TaskEntity.java
@@ -7,6 +7,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
@@ -15,5 +18,7 @@ import lombok.NoArgsConstructor;
 public class TaskEntity {
     @Id
     private String taskId;
-
+    private String taskDescription;
+    private LocalDate issuedDate;
+    private LocalTime issuedTime;
 }
