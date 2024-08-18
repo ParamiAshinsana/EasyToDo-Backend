ada322f67e57de34828aad3a4d29c28f2dafa085
Parami <paramiashinsana@gmail.com>
2024-08-18 22:08:53 +0530

    update TaskController

--- src/main/java/org/example2/todobackend/controller/TaskController.java
+++ src/main/java/org/example2/todobackend/controller/TaskController.java
@@ -1,5 +1,12 @@
 package org.example2.todobackend.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/ticket")
 public class TaskController {
 
 }
