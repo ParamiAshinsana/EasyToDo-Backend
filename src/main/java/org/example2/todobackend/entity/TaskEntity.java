50934788ccf59fb79694ff1ef1cff94a606faa3c
Parami <paramiashinsana@gmail.com>
2024-08-18 22:31:25 +0530

    update

--- src/main/java/org/example2/todobackend/entity/TaskEntity.java
+++ src/main/java/org/example2/todobackend/entity/TaskEntity.java
@@ -1,6 +1,7 @@
 package org.example2.todobackend.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -12,5 +13,7 @@ import lombok.NoArgsConstructor;
 @Entity
 @Table(name = "task")
 public class TaskEntity {
+    @Id
+    private String taskId;
 
 }
--- src/main/java/org/example2/todobackend/mapping/TaskMapping.java
+++ src/main/java/org/example2/todobackend/mapping/TaskMapping.java
@@ -1,9 +1,25 @@
 package org.example2.todobackend.mapping;
 
 import lombok.RequiredArgsConstructor;
+import org.example2.todobackend.dto.TaskDTO;
+import org.example2.todobackend.entity.TaskEntity;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 @RequiredArgsConstructor
 public class TaskMapping {
+    private final ModelMapper mapper;
+
+    public TaskDTO toTaskDTO(TaskEntity taskEntity) {
+        return  mapper.map(taskEntity, TaskDTO.class);
+    }
+    public TaskEntity toTask(TaskDTO taskDTO) {
+        return  mapper.map(taskDTO, TaskEntity.class);
+    }
+    public List<TaskDTO> toTaskDTOList(List<TaskEntity> taskEntities) {
+        return mapper.map(taskEntities, List.class);
+    }
 }
