dc18abf8403f565ced4147fddca9dab01d53b344
Parami <paramiashinsana@gmail.com>
2024-08-19 01:48:30 +0530

    update TaskDTO

--- src/main/java/org/example2/todobackend/dto/TaskDTO.java
+++ src/main/java/org/example2/todobackend/dto/TaskDTO.java
@@ -4,8 +4,15 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 public class TaskDTO {
+    private String taskId;
+    private String taskDescription;
+    private LocalDate issuedDate;
+    private LocalTime issuedTime;
 }
