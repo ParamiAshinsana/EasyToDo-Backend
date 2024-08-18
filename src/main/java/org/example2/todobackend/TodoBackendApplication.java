9f36ad75bbabe222c3dc2087afbdb10d6cb037cd
Parami <paramiashinsana@gmail.com>
2024-08-18 20:40:31 +0530

    initialize the project

---
+++ src/main/java/org/example2/todobackend/TodoBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example2.todobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoBackendApplication.class, args);
+    }
+
+}
