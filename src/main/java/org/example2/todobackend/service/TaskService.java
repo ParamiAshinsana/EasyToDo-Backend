d4d5d61baf118557639bd2a3bd7f41fba259132a
Parami <paramiashinsana@gmail.com>
2024-08-19 03:11:58 +0530

    update TaskService

--- src/main/java/org/example2/todobackend/service/TaskService.java
+++ src/main/java/org/example2/todobackend/service/TaskService.java
@@ -2,7 +2,17 @@ package org.example2.todobackend.service;
 
 import org.example2.todobackend.dto.TaskDTO;
 
+import java.util.List;
+
 public interface TaskService {
 
     TaskDTO saveTask(TaskDTO taskDTO);
+
+    void deleteTask(String id);
+
+    void updateTask(String id, TaskDTO taskDTO);
+
+    List<TaskDTO> getAllTasks();
+
+    TaskDTO getSelectedTask(String id);
 }
