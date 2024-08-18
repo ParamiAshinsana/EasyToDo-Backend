48d67b943d7965155749ba8a938661eb854e5c78
Parami <paramiashinsana@gmail.com>
2024-08-18 22:35:29 +0530

    update TaskRepository

--- src/main/java/org/example2/todobackend/repository/TaskRepository.java
+++ src/main/java/org/example2/todobackend/repository/TaskRepository.java
@@ -1,7 +1,9 @@
 package org.example2.todobackend.repository;
 
+import org.example2.todobackend.entity.TaskEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TaskRepository {
+public interface TaskRepository  extends JpaRepository<TaskEntity, String> {
 }
