b57bbc2869495d1b5199b76299c77a90bb1cf86e
Parami <paramiashinsana@gmail.com>
2024-08-19 02:09:35 +0530

    update TaskServiceIMPL

--- src/main/java/org/example2/todobackend/service/impl/TaskServiceIMPL.java
+++ src/main/java/org/example2/todobackend/service/impl/TaskServiceIMPL.java
@@ -8,6 +8,8 @@ import org.example2.todobackend.repository.TaskRepository;
 import org.example2.todobackend.service.TaskService;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.UUID;
 
 @Service
@@ -19,6 +21,17 @@ public class TaskServiceIMPL implements TaskService {
 
     @Override
     public TaskDTO saveTask(TaskDTO taskDTO) {
+
+        // Assign current date if issuedDate is not provided
+        if (taskDTO.getIssuedDate() == null) {
+            taskDTO.setIssuedDate(LocalDate.now());
+        }
+
+        // Assign current time if issuedTime is not provided
+        if (taskDTO.getIssuedTime() == null) {
+            taskDTO.setIssuedTime(LocalTime.now());
+        }
+
         taskDTO.setTaskId(UUID.randomUUID().toString());
         return taskMapping.toTaskDTO(taskRepository.save(taskMapping.toTask(taskDTO)));
     }
