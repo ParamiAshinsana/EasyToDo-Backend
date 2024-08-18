949cb59e5174868c224cea086696861d5c23bb22
Parami <paramiashinsana@gmail.com>
2024-08-18 22:35:46 +0530

    update TaskServiceIMPL

--- src/main/java/org/example2/todobackend/service/impl/TaskServiceIMPL.java
+++ src/main/java/org/example2/todobackend/service/impl/TaskServiceIMPL.java
@@ -2,10 +2,22 @@ package org.example2.todobackend.service.impl;
 
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.example2.todobackend.dto.TaskDTO;
+import org.example2.todobackend.mapping.TaskMapping;
+import org.example2.todobackend.repository.TaskRepository;
+import org.example2.todobackend.service.TaskService;
 import org.springframework.stereotype.Service;
 
 @Service
 @Transactional
 @RequiredArgsConstructor
-public class TaskServiceIMPL {
+public class TaskServiceIMPL implements TaskService {
+    private final TaskRepository taskRepository;
+    private final TaskMapping taskMapping;
+
+
+    @Override
+    public TaskDTO saveTask(TaskDTO taskDTO) {
+        return null;
+    }
 }
