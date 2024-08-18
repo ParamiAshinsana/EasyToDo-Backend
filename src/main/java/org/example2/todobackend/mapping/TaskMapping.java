b54e39221e6a850eb30b4dc36a3e724abeae6ead
Parami <paramiashinsana@gmail.com>
2024-08-18 22:14:39 +0530

    update TaskMapping

--- src/main/java/org/example2/todobackend/mapping/TaskMapping.java
+++ src/main/java/org/example2/todobackend/mapping/TaskMapping.java
@@ -1,4 +1,9 @@
 package org.example2.todobackend.mapping;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
 public class TaskMapping {
 }
