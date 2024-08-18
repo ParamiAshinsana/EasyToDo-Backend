d5b5df8fb00059045d79f7b2942ed99b6b065d7e
Parami <paramiashinsana@gmail.com>
2024-08-18 22:13:57 +0530

    update TaskEntity

--- src/main/java/org/example2/todobackend/entity/TaskEntity.java
+++ src/main/java/org/example2/todobackend/entity/TaskEntity.java
@@ -1,5 +1,16 @@
 package org.example2.todobackend.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "task")
 public class TaskEntity {
 
 }
