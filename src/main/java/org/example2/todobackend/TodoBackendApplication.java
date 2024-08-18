a05038761771a6f31345891d26bdc3b422c7d414
Parami <paramiashinsana@gmail.com>
2024-08-18 22:15:54 +0530

    update TodoBackendApplication

--- src/main/java/org/example2/todobackend/TodoBackendApplication.java
+++ src/main/java/org/example2/todobackend/TodoBackendApplication.java
@@ -1,12 +1,19 @@
 package org.example2.todobackend;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories
 public class TodoBackendApplication {
+
+    @Bean
+    ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(TodoBackendApplication.class, args);
