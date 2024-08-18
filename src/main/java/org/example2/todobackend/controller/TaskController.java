4360d61aac12e292e39e4ecb4b7c89a7ffdff8b4
Parami <paramiashinsana@gmail.com>
2024-08-19 03:11:48 +0530

    update TaskController

--- src/main/java/org/example2/todobackend/controller/TaskController.java
+++ src/main/java/org/example2/todobackend/controller/TaskController.java
@@ -3,10 +3,11 @@ package org.example2.todobackend.controller;
 import lombok.RequiredArgsConstructor;
 import org.example2.todobackend.dto.TaskDTO;
 import org.example2.todobackend.service.TaskService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,6 +19,28 @@ public class TaskController {
     @PostMapping(value = "/saveTask")
     public TaskDTO saveTasks(@RequestBody TaskDTO taskDTO){
         return taskService.saveTask(taskDTO);
+    }
+
+    @DeleteMapping(value = "/deleteTask/{id}")
+    public void deleteTasks(@PathVariable ("id") String id){
+        taskService.deleteTask(id);
+    }
+
+    @PutMapping(value = "/updateTask/{id}")
+    public void updateTasks(@RequestBody TaskDTO taskDTO, @PathVariable ("id") String id){
+        taskService.updateTask(id,taskDTO);
+        System.out.println("User Updated!");
+    }
+
+    @GetMapping(value = "/getAllTasks")
+    List<TaskDTO> getAllTasks(){
+        return taskService.getAllTasks();
+    }
+
+    @GetMapping("/getSelectedTask/{id}")
+    ResponseEntity<TaskDTO> getSelectedTask(@PathVariable ("id") String id){
+        TaskDTO selectedTask = taskService.getSelectedTask(id);
+        return selectedTask != null ? ResponseEntity.ok(selectedTask) : ResponseEntity.status(HttpStatus.NOT_FOUND).build();
     }
 
 }
