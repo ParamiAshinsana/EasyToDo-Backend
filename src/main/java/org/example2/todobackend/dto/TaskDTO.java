38d7f60a18db32a110b998b9a16f0e44b20bab71
Parami <paramiashinsana@gmail.com>
2024-08-18 22:13:42 +0530

    update TaskDTO

--- src/main/java/org/example2/todobackend/dto/TaskDTO.java
+++ src/main/java/org/example2/todobackend/dto/TaskDTO.java
@@ -1,4 +1,11 @@
 package org.example2.todobackend.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class TaskDTO {
 }
